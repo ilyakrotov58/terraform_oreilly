b2a4174be6c07f606a400a42bc2835ca4249368d
ilya_krotov <ilyakrotov58@gmail.com>
2025-04-04 17:05:20 +0400

    variables + output

--- main.tf
+++ main.tf
@@ -10,8 +10,9 @@ resource "aws_instance" "example" {
     user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
+
     user_data_replace_on_change = true
 
     tags = {
@@ -23,11 +24,22 @@ resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
 
     ingress {
-      from_port   = 8080
-      to_port     = 8080
+      from_port   = var.server_port
+      to_port     = var.server_port
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     }
+}
+
+variable "server_port" {
+    description = "Port for HTTP requests"
+    type = number
+    default = 8080
+}
+
+output "public_ip" {
+    value = aws_instance.example.public_ip
+    description = "Public IP adress of web server"
 }
 
 # To visualize the order of resource deployment, 
